6323c456e87c6be0d67be4b9c45caa8824347a1a
ditchcord <wellsjason543@gmail.com>
2026-01-12 22:51:55 -0500

    everything except chamber member processing

--- main.py
+++ main.py
@@ -2,27 +2,106 @@
 # J Wells - wellsjason543 at gmail dot com - jwellsuhhuh (Discord)
 #
 
-from datetime import datetime
-import os # for folder creation
 from os.path import isfile
 import csv
+import copy
+
+# for folder creation
+from datetime import datetime
+import os
+
+# debug only
+from cmu_cpcs_utils import prettyPrint
 
 class Member:
     def __init__(self, name: list):
-        self.name = name
-        self.pieces: list = []
+        self.name: str = name
+        self.pieces: list[tuple] = [] # (piece, instrument)
         
     def __str__(self):
         return f'{self.name}: {self.pieces}'
+    
+    def __repr__(self): # not true repr
+        return f'{self.name}: {self.pieces}' 
 
-def addSymphonyMembers(members: list[Member], symphony2dList):
-    pass
+    def addPiece(self, piece: str, instrument: str):
+        self.pieces.append((piece, instrument))
+
+pieces: list[str] = []
+
+def addSymphonyMembers(members: list[Member], symphony2dList, pieces):
+    # find all cells that end with "conductor"
+    conductorCells: list[tuple] = []
+    for rowIndex in range(0,len(symphony2dList)):
+        for colIndex in range(0,len(symphony2dList[0])):
+            if symphony2dList[rowIndex][colIndex].endswith('conductor'):
+                conductorCells.append((rowIndex, colIndex))
+                
+    # for each "conductor" cell
+    for conductorCellIndex in range(0,len(conductorCells)):
+        piece: str = symphony2dList[conductorCells[conductorCellIndex][0]-1][conductorCells[conductorCellIndex][1]]
+        pieces.append(piece)
+        currentNameCell: list = [conductorCells[conductorCellIndex][0], conductorCells[conductorCellIndex][1]+2]
+        while True: # while not yet 3 consecutive empty rows
+            if (symphony2dList[currentNameCell[0]][currentNameCell[1]] == '' and
+                symphony2dList[currentNameCell[0]+1][currentNameCell[1]] == '' and
+                symphony2dList[currentNameCell[0]+2][currentNameCell[1]] == ''):
+                break
+            
+            # get instrument pertaining to the currentNameCell
+            currentInstrumentCell: list = [currentNameCell[0], currentNameCell[1]-1]
+            while symphony2dList[currentInstrumentCell[0]][currentInstrumentCell[1]] == '':
+                currentInstrumentCell[0] -= 1 # scroll up until instrument is there
+
+            # get and filter instrument name
+            instrument: str = symphony2dList[currentInstrumentCell[0]][currentInstrumentCell[1]]
+            endIndex = instrument.find('=') - 1
+            if 0 <= endIndex: instrument = instrument[0:endIndex] # strip at " =" if applicable
+
+            # get and filter member name
+            name: str = symphony2dList[currentNameCell[0]][currentNameCell[1]]
+            endIndex = max(name.find(','), name.find(' ('))
+            if 0 <= endIndex: name = name[0:endIndex] # strip stuff after end of name
+
+            # deal with "same as above" names
+            if name.startswith("same as above"):
+                # get name of piece 2 pieces ago
+                abovePiece: str = symphony2dList[conductorCells[conductorCellIndex-2][0]-1][conductorCells[conductorCellIndex-2][1]]
+                # add every member on that instrument from 2 pieces ago to this piece
+                for member in members:
+                    for piecePlaying in member.pieces:
+                        if piecePlaying[0] == abovePiece and piecePlaying[1] == instrument:
+                            member.addPiece(piece, instrument)
+                currentNameCell[0] += 1 # go to next row
+                continue
+            
+            # add name to members
+            createNewEntry = True
+            for memberIndex in range(0,len(members)):
+                if members[memberIndex].name == name:
+                    createNewEntry = False
+                    # add piece playing to existing member
+                    members[memberIndex].addPiece(piece, instrument)
+            if createNewEntry:
+                # create new member and add piece playing
+                members.append(Member(name))
+                members[-1].addPiece(piece, instrument)
+            
+            currentNameCell[0] += 1 # go to next row
 
 def addChamberMembers(members: list[Member], chamber2dList):
     pass
 
-def createPieces2dList(members):
-    pass
+def createPieces2dList(members: list[Member], pieces: list[str]):
+    pieces2dList: list[list] = [pieces] + [[''] * len(pieces) for _ in range(len(members))]
+    for memberIndex in range(0,len(members)): # each member
+        for pieceIndex in range(0,len(pieces)): # each of all pieces
+            for piecePlayingIn in members[memberIndex].pieces: # each piece the member is playing in
+                if piecePlayingIn[0] == pieces[pieceIndex]:
+                    # insert instrument name into the index for the piece they're playing in
+                    pieces2dList[memberIndex+1][pieceIndex] = piecePlayingIn[1]
+    
+    return pieces2dList   
 
 def getFilename(prompt: str) -> str:
     while True:
@@ -47,32 +126,39 @@ def createFolderInPrivate() -> str:
 
 def main():
     members: list[Member] = []
+    pieces: list[str] = []
 
     # Process symphony roster
     symphonyFilename = getFilename('Symphony Roster Filename: ')
     with open(symphonyFilename) as file:
         symphony2dList: list[list] = list(csv.reader(file, delimiter='\t'))
-    addSymphonyMembers(members, symphony2dList)
+    addSymphonyMembers(members, symphony2dList, pieces)
 
     # Process chamber roster
     chamberFilename = getFilename('Chamber Orchestra Roster Filename: ')
     with open(chamberFilename) as file:
         chamber2dList: list[list] = list(csv.reader(file, delimiter='\t'))
-    addChamberMembers(members, chamber2dList)
+    # addChamberMembers(members, chamber2dList, pieces)
 
     # Process board member list
     boardFilename = getFilename('Board Members List Filename: ')
+    boardMembersList: list = []
     with open(boardFilename) as file:
-        boardMembersList: list = file.readlines()
-
-    # Move board members to beginning of members list
-    memberIndex = 0
-    while memberIndex < len(members): # loop through every member
-        memberName = members[memberIndex].name
-        if memberName in boardMembersList:
-            members.insert(members[memberIndex], 0) # move it to beginning of list
-            del members[memberIndex+1]
-        memberIndex += 1
+        for line in file.readlines():
+            boardMembersList.append(line.strip())
+        
+    # recreate the member list with filters
+    oldMembers = copy.deepcopy(members)
+    members: list[Member] = []
+    for member in oldMembers:
+        # skip "guest musician" and "" members
+        if member.name.startswith('Guest Musician') or member.name == '':
+            continue
+        # move board members to beginning of member list
+        if member.name in boardMembersList:
+            members.insert(0, member)
+        # append rest of members as usual
+        else: members.append(member)
 
     folderPath = createFolderInPrivate()
 
@@ -80,11 +166,11 @@ def main():
     filename = 'names.txt'
     with open(f'{folderPath}/{filename}', 'w') as file:
         for member in members:
-            file.write(member.name)
+            file.write(f'{member.name}\n')
     
     # export Member pieces.txt
     filename = 'pieces.txt'
-    pieces2dList = createPieces2dList(members)
+    pieces2dList = createPieces2dList(members, pieces)
     with open(f'{folderPath}/{filename}', 'w') as file:
         csvWriter = csv.writer(file,delimiter=',')
         csvWriter.writerows(pieces2dList)
